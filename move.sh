282f52f24f7e390a1e0e87ec79d3f38f55cf1710
ben44496 <ben44496@gmail.com>
2021-09-10 16:41:55 -0500

    Update 9/10/21

--- move.sh
+++ move.sh
@@ -4,9 +4,10 @@ mv 498dv-* 498dv/
 mv 498gc-* 498gc/
 mv 211-* 211/
 mv 441-* 441/
+mv parasol-* parasol/
 
 # Loop through the sub directory scripts and run them
-declare -a Scripts=("./374/move.sh ./498dv/move.sh ./498gc/move.sh ./441/move.sh ./211/move.sh")
+declare -a Scripts=("./374/move.sh ./498dv/move.sh ./498gc/move.sh ./441/move.sh ./211/move.sh ./parasol/move.sh")
 
 for script in ${Scripts[@]}; do
     echo $script
