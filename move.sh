628a2258d779d5b6649335a7762ccd520109619c
ben44496 <ben44496@gmail.com>
2021-09-09 23:46:31 -0500

    Add script to run sub-scripts

--- move.sh
+++ move.sh
@@ -1,4 +1,15 @@
+# Move any new files in the home directory to their respective directories
 mv 374-* 374/
 mv 498dv-* 498dv/
 mv 498gc-* 498gc/
 mv 211-* 211/
+mv 441-* 441/
+
+# Loop through the sub directory scripts and run them
+declare -a Scripts=("./374/move.sh ./498dv/move.sh ./498gc/move.sh ./441/move.sh ./211/move.sh")
+
+for script in ${Scripts[@]}; do
+    echo $script
+    chmod +x $script
+    $script
+done
